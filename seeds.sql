04320307442ddf88268ed519c0245dc32972a07d
Gunslinger <jtgeislinger@gmail.com>
2019-02-16 22:25:11 -0500

    update

--- seeds.sql
+++ seeds.sql
@@ -1,49 +1,37 @@
 USE bamazonDB;
 
 INSERT INTO products (product_name, departmant_name,price, stock_quantity)
-VALUES ("shoes", 'men', 95, 10);
+VALUES ("laptop", 'electronics', 2000, 10);
 
 INSERT INTO products (product_name, departmant_name,price, stock_quantity)
-VALUES ("purse", 'women', 50, 5);
+VALUES ("cellphone", 'electronics', 750, 40);
 
 INSERT INTO products (product_name, departmant_name,price, stock_quantity)
-VALUES ("wallet", 'men', 70, 15);
+VALUES ("tv", 'electronics', 550, 25);
 
 INSERT INTO products (product_name, departmant_name,price, stock_quantity)
-VALUES ("shirt", 'men', 66, 7);
+VALUES ("shirt", 'men', 45, 15);
 
 INSERT INTO products (product_name, departmant_name,price, stock_quantity)
-VALUES ("cologne", 'men', 105, 11);
+VALUES ("shoes", 'men', 75, 50);
 
-INSERT INTO products
-    (product_name, departmant_name,price, stock_quantity)
-  VALUES
-    ("body spray", 'women', 125, 9);
+INSERT INTO products (product_name, departmant_name,price, stock_quantity)
+VALUES ("shorts", 'men', 25, 30);
 
-INSERT INTO products
-    (product_name, departmant_name,price, stock_quantity)
-  VALUES
-    ("skirt", 'women', 55, 17);
+INSERT INTO products (product_name, departmant_name,price, stock_quantity)
+VALUES ("belt", 'men', 20, 20);
 
-INSERT INTO products
-    (product_name, departmant_name,price, stock_quantity)
-  VALUES
-    ("dress", 'women', 45, 12);
+INSERT INTO products (product_name, departmant_name,price, stock_quantity)
+VALUES ("dress", 'women', 45, 20);
 
-INSERT INTO products
-    (product_name, departmant_name,price, stock_quantity)
-  VALUES
-    ("belt", 'men', 30, 14);
+INSERT INTO products (product_name, departmant_name,price, stock_quantity)
+VALUES ("skirt", 'women', 55, 25);
 
-INSERT INTO products
-    (product_name, departmant_name,price, stock_quantity)
-  VALUES
-    ("shampoo", 'beauty', 6, 10);
+INSERT INTO products (product_name, departmant_name,price, stock_quantity)
+VALUES ("heels", 'women', 60, 10);
 
-INSERT INTO products
-    (product_name, departmant_name,price, stock_quantity)
-  VALUES
-    ("tooth paste", 'beauty', 5, 10);
+INSERT INTO products (product_name, departmant_name,price, stock_quantity)
+VALUES ("bathsuit", 'women', 85, 10);
 
 USE bamazonDB;
 
