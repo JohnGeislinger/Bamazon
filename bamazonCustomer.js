f4389f4cedb88b5c92abc0fa0a38bd554b47cf6a
Gunslinger <jtgeislinger@gmail.com>
2019-04-23 15:51:49 -0400

    corrected an issue in bamazonCustomer.js file and completed Readme

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,11 @@
+// Dependencies
 require("dotenv").config();
 
 const mysql = require("mysql");
 const PASSWORD = process.env.PASSWORD;
 const inquirer = require("inquirer");
 
-let customerChosenId = 0;
-let customerQuantity = 0;
-let customerTotalQuantity = 0;
-let customerPrice = 0;
-
-// MySQL connection
+// MySQL Connection
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -24,75 +20,107 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
-//connect to the database function 
+// Connect to the Database 
 connection.connect(function (err) {
     if (err) throw err;
-    //console.log("connected as id " + connection.threadId);
-    questions();
+    products();
 });
 
-function questions() {
+// Load the Products
+function products() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
         console.table(res);
 
-        inquirer
-            .prompt([
-                {
-                    name: "item_id",
-                    type: "input",
-                    message: "Please enter the ID of the product that you wish to buy:"
-                },
-                {
-                    name: "quantity",
-                    type: "input",
-                    message: "How many of this product would you like to purchase?"
-                }
-            ]).then(function (answer) {
-                customerChosenId = parseInt(answer.item_id);
-                customerQuantity = parseInt(answer.customerQuantity);
-                customerPrice = res[customerChosenId - 1].price;
-                customerTotalQuantity = res[customerChosenId - 1].stock_quantity - customerQuantity;
-                console.log(customerTotalQuantity);
-                getQuantityProduct(customerChosenId);
-            });
-    })
+        promptForId(res);
+    });
 };
 
-function getQuantityProduct(customerChosenId) {
-    connection.query("SELECT stock_quantity FROM products WHERE ?",
-    {
-        item_id: customerChosenId
-    },
-    function(err, res) {
+// Asking for the ID of the Item
+function promptForId(inventory) {
+    inquirer
+        .prompt([
+            {
+                name: "choice",
+                type: "input",
+                message: "Please enter the ID of the product that you wish to buy: [Press Q to Quit]",
+                validate: function(val) {
+                    return !isNaN(val) || val.toLowerCase() === "q";
+                }
+            }   
+        ])
+        .then(function(val) {
+            exit(val.choice);
+            let chosenId = parseInt(val.choice);
+            let product = checkInventory(chosenId, inventory);
+                
+            if (product) {
+                promptForQuantity(product);
+            }
+            else {
+                console.log("That item is not in the inventory.");
+                products();
+            }
+        });
+};
+
+// Asking for the Quantity
+function promptForQuantity(product) {
+    inquirer
+        .prompt([
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many of this product would you like to purchase: [Press Q to Quit]",
+                validate: function(val) {
+                    return val > 0 || val.toLowerCase() === "q";
+                }
+            }
+        ])
+        .then(function(val) {
+            exit(val.quantity);
+            let quantity = parseInt(val.quantity);
+
+            if (quantity > product.stock_quantity) {
+                console.log("Insufficient quantity to fulfill your order.")
+                products();
+            }
+            else {
+                purchase(product, quantity);
+            }
+        });
+};
+
+// Update the Database and Give the Customer the Total
+function purchase(product, quantity) {
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [quantity, product.item_id], function(err, res) {
         if (err) throw err;
 
-        if (customerQuantity > res[0].stock_quantity) {
-            console.log("Insufficient Quantity!");
-        } else if (customerQuantity < res[0].stock_quantity) {
-            updateProductTable(customerTotalQuantity, customerChosenId);
-        }
-    })
-}
+        let productPrice = product.price;
+        let customerTotal = quantity * productPrice;
+        
+        console.log("You purchased (" + quantity + ") quantity of " + product.product_name + "!");
+        console.log("Your total is $" + customerTotal + "!");
+        promptForId();
+    });
+};
 
-function updateProductTable(customerTotalQuantity, customerChosenId) {
-    connection.query("UPDATE products SET ? WHERE ?",
-        [ 
-            {
-                stock_quantity: customerTotalQuantity
-            },
-            {
-                item_id: customerChosenId
-            }
-        ],
-        function (err, res) {
-            if (err) throw err;
-
-            let customerTotal = customerQuantity * customerPrice;
-            console.log("Your total price is $" + customerTotal);
+// Function to Check the Inventory
+function checkInventory(chosenId, inventory) {
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].item_id === chosenId) {
+            return inventory[i];
         }
-    
-    
-    )}
+    }
+    return null;
+};
+
+// Function to Check is the Customer wants to Quit the Program
+function exit(choice) {
+    if (choice.toLowerCase() === "q") {
+        console.log("Thanks for shopping Bamazon!");
+        process.exit(0);
+    }
+};
     
